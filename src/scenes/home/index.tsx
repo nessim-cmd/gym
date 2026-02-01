3bc2cdebf3ae39501cbbcb54decbf38d8f71065a
nessim-cmd <bennasrnessim@outlook.com>
2026-02-01 10:07:22 +0100

    Finish project

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -11,20 +11,20 @@ import { motion } from "framer-motion";
 
 
 type Props = {
-    setSelectedPage:(value: SelectedPage)=> void;
+    setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Home = ({setSelectedPage} : Props) =>{
+const Home = ({ setSelectedPage }: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
-   
-   
-    return(
+
+
+    return (
         <section
-        id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
+            id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0"
         >
             {/*Image and main header */}
             <motion.div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
-            onViewportEnter={()=> setSelectedPage(SelectedPage.Home)}
+                onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
             >
                 {/**MAin header */}
                 <div className="z-10 mt-32 md:basis-3/5">
@@ -32,39 +32,39 @@ const Home = ({setSelectedPage} : Props) =>{
                     <motion.div className="md:-mt-20"
                         initial="hidden"
                         whileInView="visible"
-                        viewport={{once: true, amount: 0.5}}
-                        transition={{duration: 0.5}}
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
                         variants={{
-                            hidden:{opacity: 0, x:-50},
-                            visible:{opacity: 1, x:0},
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
                         }}>
                         <div className="relative">
                             <div className="before:absolute before:-top-20 before:left-20 before:z-[-1] md:before:content-evolvetext">
                                 <img src={homePageText} alt="home-page-text" />
                             </div>
                         </div>
-                        <p className="mt-8 text-sm ">
-                        Welcome Welcome Welcome Welcome Welcome 
-                        Welcome Welcome Welcome Welcome 
-                        Welcome Welcome Welcome 
+                        <p className="mt-8 text-sm">
+                            Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+                            Studios to get the Body Shapes That you Dream of.. Get Your Dream
+                            Body Now.
                         </p>
                     </motion.div>
                     {/**action */}
                     <motion.div className="mt-8 flex items-center gap-8 "
                         initial="hidden"
                         whileInView="visible"
-                        viewport={{once: true, amount: 0.5}}
-                        transition={{delay:0.2, duration: 0.5}}
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
                         variants={{
-                            hidden:{opacity: 0, x:-50},
-                            visible:{opacity: 1, x:0},
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
                         }}>
-                        <ActionButton setSelectedPage={setSelectedPage}> 
+                        <ActionButton setSelectedPage={setSelectedPage}>
                             Join Now
                         </ActionButton>
                         <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
-                        onClick={()=> setSelectedPage(SelectedPage.ContactUs)}
-                        href={`#${SelectedPage.ContactUs}`}>
+                            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                            href={`#${SelectedPage.ContactUs}`}>
                             <p>Learn More</p>
                         </AnchorLink>
                     </motion.div>
@@ -75,14 +75,14 @@ const Home = ({setSelectedPage} : Props) =>{
                 </div>
             </motion.div>
             {/*Sponsors */}
-            {isAboveMediumScreens &&(
+            {isAboveMediumScreens && (
                 <div className="h-[150px] w-full bg-primary-100 py-10" >
                     <div className="mx-auto w-5/6">
-                    <div className="flex w-3/5 items-center justify-between gap-8">
-                        <img src={SponsorRedBull} alt="SponsorRedBull" />
-                        <img src={SponsorFortune} alt="SponsorFortune" />
-                        <img src={SponsorForbes} alt="SponsorForbes" />
-                    </div>
+                        <div className="flex w-3/5 items-center justify-between gap-8">
+                            <img src={SponsorRedBull} alt="SponsorRedBull" />
+                            <img src={SponsorFortune} alt="SponsorFortune" />
+                            <img src={SponsorForbes} alt="SponsorForbes" />
+                        </div>
                     </div>
                 </div>
             )}
